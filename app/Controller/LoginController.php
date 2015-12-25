b8eb2d87a236b6e61c3b04da280965e7dcb52701
mharasawa <m.harasawa@movsquad.com>
2015-12-25 12:59:14 +0900

    ログインページ追加

---
+++ app/Controller/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+App::uses('AppController', 'Controller');
+App::uses('UserAddress', 'Model');
+
+class LoginController extends AppController
+{
+    /**
+     * 制御前段処理.
+     */
+    public function beforeFilter()
+    {
+        AppController::beforeFilter();
+    }
+
+    /**
+     * ルートインデックス.
+     */
+    public function index()
+    {
+    }
+}
