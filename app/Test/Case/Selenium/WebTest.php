bfcd0a11ba28947a8225ac9e5cab99435d7afb2e
t.koike <t.koike@movsquad.com>
2016-01-19 21:27:56 +0900

    phpunit-selenimを追加

---
+++ app/Test/Case/Selenium/WebTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'MinikuraTestCase.php';
+
+class WebTest extends MinikuraTestCase {
+
+    public function testTitle() {
+        $this->url('/');
+        $this->waitPageLoad();
+
+        $this->assertEquals('minikura | minikura', $this->title());
+    }
+}
