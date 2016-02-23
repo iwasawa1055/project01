6c0e20adf2602049971d54d9c95675911b2b3ecb
t.koike <t.koike@movsquad.com>
2016-02-23 22:44:24 +0900

    2重クリック防止対策

--- app/webroot/js/app_dev.js
+++ app/webroot/js/app_dev.js
@@ -1,18 +1,23 @@
 // animsition
 $(function() {
-    $('a[href^="/"]a[target!="_blank"]').addClass('animsition-link');
-    $('.animsition').animsition();
+  $('a[href^="/"]a[target!="_blank"]').addClass('animsition-link');
+  $('button[type=submit]').addClass('page-transition-link');
+  $('.animsition').animsition();
 
-    $('.agree-before-submit[type="checkbox"]').click(checkAgreeBeforeSubmit);
-    checkAgreeBeforeSubmit();
+  $('.agree-before-submit[type="checkbox"]').click(checkAgreeBeforeSubmit);
+  checkAgreeBeforeSubmit();
+
+  $('form').submit(function() {
+    $('button[type=submit]', this).attr('disabled', 'true');
+  });
 });
 
 function checkAgreeBeforeSubmit() {
   var count = $('.agree-before-submit[type="checkbox"]').length;
   if (0 < count) {
-    $('button[type=submit]').attr('disabled', 'true');
+    $('#page-wrapper button[type=submit]').attr('disabled', 'true');
     if (count === $('.agree-before-submit[type="checkbox"]:checked').length) {
-      $('button[type=submit]').attr('disabled', null);
+      $('#page-wrapper button[type=submit]').attr('disabled', null);
     }
   }
 }
