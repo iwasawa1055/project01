befc54f73c9b5431f47df6310ca0f4ec6bdda477
mharasawa <m.harasawa@movsquad.com>
2016-03-01 17:52:29 +0900

    お知らせ lifetime を変更

--- app/Model/Announcement.php
+++ app/Model/Announcement.php
@@ -11,7 +11,7 @@ class Announcement extends ApiCachedModel
 
     public function __construct()
     {
-        parent::__construct(self::SESSION_CACHE_KEY, 60, 'Announcement', '/announcement');
+        parent::__construct(self::SESSION_CACHE_KEY, 5, 'Announcement', '/announcement');
     }
 
     public $validate = [
