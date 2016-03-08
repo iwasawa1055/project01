d49e47b6a4a479b087861a61b486dfa23f6d714f
t.koike <t.koike@movsquad.com>
2016-03-08 17:27:53 +0900

    ソート処理を修正

--- app/Model/HashSorter.php
+++ app/Model/HashSorter.php
@@ -9,16 +9,17 @@ class HashSorter
     }
     public function cmp($a, $b)
     {
-        $result = 1;
+        $result = 0;
         foreach ($this->sortKeyList as $key => $isAsc) {
 
             $aValue = Hash::get($a, $key);
             $bValue = Hash::get($b, $key);
-            if (!empty($aValue) && !empty($bValue) && $aValue !== $bValue) {
-                $result = strcmp($aValue, $bValue);
-                break;
-            } else if (empty($aValue) || empty($bValue)) {
-                $result = empty($aValue) ? 1 : -1;
+            if ($aValue !== $bValue) {
+                if (!empty($aValue) && !empty($bValue)) {
+                    $result = strcmp($aValue, $bValue);
+                } else {
+                    $result = empty($aValue) ? 1 : -1;
+                }
                 break;
             }
         }
