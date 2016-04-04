da0cf2d5f8f1f17039a0b8686c634e25b75bd126
mharasawa <m.harasawa@movsquad.com>
2016-04-04 11:59:14 +0900

    いきなり本登録

--- app/Controller/Customer/RegisterController.php
+++ app/Controller/Customer/RegisterController.php
@@ -5,6 +5,7 @@ App::uses('MinikuraController', 'Controller');
 class RegisterController extends MinikuraController
 {
     const MODEL_NAME = 'CustomerEntry';
+    const MODEL_NAME_REGIST = 'CustomerRegistInfo';
 
     // ログイン不要なページ
     protected $checkLogined = false;
@@ -66,6 +67,75 @@ class RegisterController extends MinikuraController
                 return $this->render('customer_add');
             }
 
+        }
+    }
+
+    /**
+     *
+     */
+    public function customer_add_info()
+    {
+        // 紹介コード
+        $code = Hash::get($this->request->query, 'code');
+        $this->set('code', $code);
+
+        if ($this->request->is('post')) {
+            $this->loadModel(self::MODEL_NAME_REGIST);
+            $data = $this->request->data[self::MODEL_NAME_REGIST];
+
+            $birth = [];
+            $birth[0] = $data['birth_year'];
+            $birth[1] = $data['birth_month'];
+            $birth[2] = $data['birth_day'];
+            $data['birth'] = implode('-', $birth);
+
+            $this->CustomerRegistInfo->set($data);
+
+            // 紹介コード
+            if (!empty($code)) {
+                $this->CustomerRegistInfo->data[self::MODEL_NAME_REGIST]['alliance_cd'] = $code;
+            }
+
+            if ($this->CustomerRegistInfo->validates()) {
+                // 部屋番号
+                $this->CustomerRegistInfo->data[self::MODEL_NAME_REGIST]['address3'] = $data['address3'] . $data['room'];
+
+                // 本登録
+                $res = $this->CustomerRegistInfo->regist();
+                if (!empty($res->error_message)) {
+                    $this->request->data[self::MODEL_NAME_REGIST]['password'] = '';
+                    $this->request->data[self::MODEL_NAME_REGIST]['password_confirm'] = '';
+                    $this->Flash->set($res->error_message);
+                    return $this->render('customer_add_info');
+                }
+                
+                // ログイン
+                $this->loadModel('CustomerLogin');
+                $this->CustomerLogin->data['CustomerLogin']['email'] = $this->request->data[self::MODEL_NAME_REGIST]['email'];
+                $this->CustomerLogin->data['CustomerLogin']['password'] = $this->request->data[self::MODEL_NAME_REGIST]['password'];
+                
+                $res = $this->CustomerLogin->login();
+                if (!empty($res->error_message)) {
+                    $this->Flash->set($res->error_message);
+                    return $this->render('customer_add_info');
+                }
+                
+                // カスタマー情報を取得しセッションに保存
+                $this->Customer->setTokenAndSave($res->results[0]);
+                $this->Customer->setPassword($this->CustomerLogin->data['CustomerLogin']['password']);
+                $this->Customer->getInfo();
+                
+                if (empty($code)) {
+                    return $this->redirect('/');
+                } else {
+                    // 紹介コードがある場合はキット購入へ遷移
+                    return $this->redirect(['controller' => 'order', 'action' => 'add', 'customer' => false]);
+                }
+            } else {
+                $this->request->data[self::MODEL_NAME_REGIST]['password'] = '';
+                $this->request->data[self::MODEL_NAME_REGIST]['password_confirm'] = '';
+                return $this->render('customer_add_info');
+            }
         }
     }
 }
---
+++ app/Model/Api/CustomerRegistInfo.php
@@ -0,0 +1,238 @@
+<?php
+
+App::uses('ApiModel', 'Model');
+App::uses('Announcement', 'Model');
+
+class CustomerRegistInfo extends ApiModel
+{
+    public function __construct()
+    {
+        parent::__construct('CustomerRegistInfo', '/customer', 'minikura_v5');
+    }
+
+    protected function triggerDataChanged()
+    {
+        parent::triggerDataChanged();
+        (new Announcement())->deleteCache();
+    }
+
+    public function regist()
+    {
+        $this->data[$this->model_name]['oem_key'] = $this->oem_key;
+        $responses = $this->request('/customer', $this->data[$this->model_name], 'POST');
+        // api error
+        if (empty($responses->error_message)) {
+        } else {
+            $responses->error_message = 'ユーザー登録できませんでした。';
+        }
+
+        return $responses;
+    }
+
+    public $validate = [
+        'lastname' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'lastname']
+            ],
+            'maxLength' => [
+                'rule' => ['maxLength', 29],
+                'message' => ['maxLength', 'lastname', 29]
+            ],
+        ],
+        'lastname_kana' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'lastname_kana']
+            ],
+            'maxLength' => [
+                'rule' => ['maxLength', 29],
+                'message' => ['maxLength', 'lastname_kana', 29]
+            ],
+            'isFwKana' => [
+                'rule' => 'isFwKana',
+                'message' => ['isFwKana', 'lastname_kana']
+            ],
+        ],
+        'firstname' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'firstname']
+            ],
+            'maxLength' => [
+                'rule' => ['maxLength', 29],
+                'message' => ['maxLength', 'firstname', 29]
+            ],
+        ],
+        'firstname_kana' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'firstname_kana']
+            ],
+            'maxLength' => [
+                'rule' => ['maxLength', 29],
+                'message' => ['maxLength', 'firstname_kana', 29]
+            ],
+            'isFwKana' => [
+                'rule' => 'isFwKana',
+                'message' => ['isFwKana', 'firstname_kana']
+            ],
+        ],
+        'gender' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'gender'],
+            ],
+            'allowedChoice' => [
+                'rule' => ['inList', ['m', 'f']],
+                'message' => ['format', 'gender'],
+            ],
+        ],
+        'birth' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'birth'],
+            ],
+            'isDate' => [
+                'rule' => 'isDate',
+                'message' => ['format', 'birth'],
+            ],
+        ],
+        'tel1' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'tel1']
+            ],
+            'isPhoneNumberJp' => [
+                'rule' => 'isPhoneNumberJp',
+                'message' => ['format', 'tel1']
+            ],
+        ],
+        'email' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'email'],
+             ],
+            'isMailAddress' => [
+                'rule' => 'isMailAddress',
+                'message' => ['format', 'email'],
+            ],
+        ],
+        'password' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'password'],
+             ],
+            'isLoginPassword' => [
+                'rule' => 'isLoginPassword',
+                'message' => ['format', 'password'],
+            ],
+        ],
+        'password_confirm' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'password_confirm'],
+             ],
+            'isLoginPassword' => [
+                'rule' => 'isLoginPassword',
+                'message' => ['format', 'password_confirm'],
+            ],
+            'confirmPassword' => [
+                'rule' => 'confirmPassword',
+                'message' => ['confirm', 'password_confirm'],
+            ],
+        ],
+        'postal' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'postal']
+            ],
+            'isPostalCodeJp' => [
+                'rule' => 'isPostalCodeJp',
+                'message' => ['format', 'postal']
+            ],
+        ],
+        'pref' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'pref']
+            ],
+            'isPrefNameJp' => [
+                'rule' => 'isPrefNameJp',
+                'message' => ['format', 'pref']
+            ],
+        ],
+        'address1' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'address1']
+            ],
+            'maxLength' => [
+                'rule' => ['maxLength', 8],
+                'message' => ['maxLength', 'address1', 8]
+            ],
+        ],
+        'address2' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'address2']
+            ],
+            'maxLength' => [
+                'rule' => ['maxLength', 18],
+                'message' => ['maxLength', 'address2', 18]
+            ],
+        ],
+        'address3' => [
+            'maxLength' => [
+                'rule' => ['maxLength', 30],
+                'allowEmpty' => true,
+                'message' => ['maxLength', 'address3', 30]
+            ],
+        ],
+        'room' => [
+            'maxLengthRoom' => [
+                'rule' => 'maxLengthRoom',
+                'message' => ['maxLength', 'newsletter', 30]
+            ],
+        ],
+        'newsletter' => [
+            'allowedChoice' => [
+                'rule' => ['inList', ['0', '1']],
+                'message' => ['format', 'newsletter']
+            ],
+        ],
+    ];
+
+    public function maxLengthRoom()
+    {
+        $address3 = $this->data[$this->model_name]['address3'] . $this->data[$this->model_name]['room'];
+        if (30 < mb_strlen($address3)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function confirmPassword()
+    {
+        if ($this->data[$this->model_name]['password'] !== $this->data[$this->model_name]['password_confirm']) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
