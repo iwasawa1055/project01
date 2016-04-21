070ebae37c7943bd044ec21ba5d266cc8bbe2c1c
kyow.osada <osada.kyouji@terrada.co.jp>
2016-04-21 19:08:37 +0900

    一時的に検証環境で開発データをまわす

--- app/Config/EnvConfig/Staging/AppConfig.php
+++ app/Config/EnvConfig/Staging/AppConfig.php
@@ -24,8 +24,13 @@ error_reporting(E_ALL);
 /**
  * API設定
  */
-$config['api.minikura.oem_key'] = '_QfNQL67x7RiIUbzn_1hkAEGKmlLO04we5rizUemfoWVLOjcEpIDDQ--';
-$url = 'https://stag-api.minikura.com';
+
+# stag
+//$config['api.minikura.oem_key'] = '_QfNQL67x7RiIUbzn_1hkAEGKmlLO04we5rizUemfoWVLOjcEpIDDQ--';
+//$url = 'https://b-api.minikura.com';
+# dev
+$config['api.minikura.oem_key'] = 'mB9JCKud0_o_yQgYYhulLTpuR9plqU5BjkXU9pgb_tiyn16xwfxpSA--';
+$url = 'https://a-api.minikura.com';
 $config['api.minikura.access_point.minikura_v3'] = $url . '/v3/warehouse/minikura';
 $config['api.minikura.access_point.minikura_v4'] = $url . '/v4/minikura';
 $config['api.minikura.access_point.minikura_v5'] = $url . '/v5/minikura';
