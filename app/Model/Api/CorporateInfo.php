83be93ddbd23a250458863ac12474005667e4006
mharasawa <m.harasawa@movsquad.com>
2016-04-27 12:51:57 +0900

    法人情報変更時のapi呼び出しを追加

--- app/Model/Api/CorporateInfo.php
+++ app/Model/Api/CorporateInfo.php
@@ -15,6 +15,11 @@ class CorporateInfo extends ApiModel
         (new Announcement())->deleteCache();
     }
 
+    public function apiPatch($data)
+    {
+        return parent::apiPut($data);
+    }
+
     public $validate = [
         'company_name' => [
             'notBlank' => [
