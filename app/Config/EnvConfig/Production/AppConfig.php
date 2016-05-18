cd132248a715a05fab357bbc5d14024c084d6f66
mharasawa <m.harasawa@movsquad.com>
2016-05-18 19:20:38 +0900

    refs #8043 検証用・本番用の設定ファイルを更新
    ポイント API のエンドポイントを、検証および本番の設定ファイルへ追加

--- app/Config/EnvConfig/Production/AppConfig.php
+++ app/Config/EnvConfig/Production/AppConfig.php
@@ -30,6 +30,7 @@ $config['api.minikura.access_point.minikura_v3'] = $url . '/v3/warehouse/minikur
 $config['api.minikura.access_point.minikura_v4'] = $url . '/v4/minikura';
 $config['api.minikura.access_point.minikura_v5'] = $url . '/v5/minikura';
 $config['api.minikura.access_point.gmopayment_v4'] = $url . '/v4/gmo_payment';
+$config['api.minikura.access_point.cpss_v5'] = $url . '/v5/cpss';
 
 /**
  * nike_snkrs用
