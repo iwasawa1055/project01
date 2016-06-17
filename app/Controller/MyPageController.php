42ccb9b73269e6643197855b121002a2d7bbdc2e
goto <goto.masayuki@terrada.co.jp>
2016-06-17 10:12:53 +0900

    mockv22 マイページの改修 //box,item情報のカット //契約情報一覧をマイページに表示

--- app/Controller/MyPageController.php
+++ app/Controller/MyPageController.php
@@ -15,6 +15,7 @@ class MyPageController extends MinikuraController
         $boxList = [];
         $itemList = [];
         if (!$this->Customer->isEntry()) {
+			/* hotfix_mypage_v22 最近預けたボックスとアイテムをカット、代わりに契約情報を差し込む [start]
             // 最近預けたボックスとアイテム
             $box = new InfoBox();
             $list = $box->getListLastInbound();
@@ -22,13 +23,18 @@ class MyPageController extends MinikuraController
             $item = new InfoItem();
             $list = $item->getListLastInbound();
             $itemList =  array_slice($list, 0, 10);
+			// hotfix_mypage_v22 最近預けたボックスとアイテムをカット、代わりに契約情報を差し込む [end]*/
+			//* 契約情報
         }
 
         $News = new News();
         $newsList = $News->getNews(2);
 
         $this->set('newsList', $newsList);        
+		/* hotfix_mypage_v22 最近預けたボックスとアイテムをカット、代わりに契約情報を差し込む [start]
         $this->set('boxList', $boxList);
         $this->set('itemList', $itemList);
+		// hotfix_mypage_v22 最近預けたボックスとアイテムをカット、代わりに契約情報を差し込む [end]*/
+		 
     }
 }
