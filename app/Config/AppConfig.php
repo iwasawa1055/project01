0d7a4e18fb0379f4910be48908352fe3008fb5f9
maekawa <kazuhiro-maekawa@techbeans.co.jp>
2016-06-22 20:10:40 +0900

    コンフィグ修正

--- app/Config/AppConfig.php
+++ app/Config/AppConfig.php
@@ -18,8 +18,7 @@ $config['site.env_name'] = '';
  * API設定
  */
 $config['api.minikura.oem_key'] = 'mB9JCKud0_o_yQgYYhulLTpuR9plqU5BjkXU9pgb_tiyn16xwfxpSA--';
-//$url = 'https://a-api.minikura.com';
-$url = 'https://maekawa-api.minikura.com';
+$url = 'https://a-api.minikura.com';
 $config['api.minikura.access_point.minikura_v3'] = $url . '/v3/warehouse/minikura';
 $config['api.minikura.access_point.minikura_v4'] = $url . '/v4/minikura';
 $config['api.minikura.access_point.minikura_v5'] = $url . '/v5/minikura';
