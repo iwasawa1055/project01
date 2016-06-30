fa40515479f38dadb148c0839e5e61ba00270087
goto.masayuki <goto.masayuki@terrada.co.jp>
2016-06-30 11:51:51 +0900

    mock22 追加修正 console.log消し //購入ページで、商品説明を画像でmodal表示する

--- app/webroot/js/app.js
+++ app/webroot/js/app.js
@@ -27,7 +27,6 @@ $(function() {
     });
 
     var url = window.location;
-	console.log(url);
     var element = $('ul.nav a').filter(function() {
 		//* comment outi, this is  mock ver
         //return this.href == url || url.href.indexOf(this.href) == 0;
