5a047ff519c7462803cce01e68df4288dc744478
wada <wada.nobuya@terrada.co.jp>
2017-03-17 17:54:37 +0900

    ・デザインUI動作実装

--- app/webroot/first_order_file/js/first_order/add_order.js
+++ app/webroot/first_order_file/js/first_order/add_order.js
@@ -3,9 +3,15 @@ var AppAddOrder =
     a: function () {
         // ボックス数選択
         $('.js-item-number').change(function () {
+
+            // nameと同じ固有識別子をつかう
             var selector = $(this).data("box_type");
+
+            // console.log('selector:' + selector);
+
             var number = Number(0);
 
+            // セレクトボックスの値を取得
             $('.js-item-'+ selector).each(function () {
                 var set_number = $(this).val();
                 var selector_name = $(this).data("name");
@@ -14,10 +20,16 @@ var AppAddOrder =
                 // console.log('number:' + number);
             });
 
+            // 表示個数を変更 select_＋nameで指定
             if (number === 0) {
                 $('#select_' + selector).html('未選択');
             } else {
                 $('#select_' + selector).html('<span>' +  number +'個選択済み</span>');
+
+                // hako_limited_ver1の場合は、単位はパック
+                if (selector === 'hako_limited_ver1') {
+                    $('#select_' + selector).html('<span>' +  number +'パック選択済み</span>');
+                }
             }
         });
     },
