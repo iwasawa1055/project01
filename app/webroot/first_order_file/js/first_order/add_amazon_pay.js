3539f9836e0711e176cb068d1267b88d3e5c84b7
izumi <izumi.natsumi@terrada.co.jp>
2017-09-12 16:46:01 +0900

    アマゾンペイメント対応 ・同意ウィジェットのバリデーション表示不具合修正

--- app/webroot/first_order_file/js/first_order/add_amazon_pay.js
+++ app/webroot/first_order_file/js/first_order/add_amazon_pay.js
@@ -16,8 +16,8 @@ var AppAmazonPay =
                 return;
             }
 
-            if( !$('div.dev-divider').prev('div').children('span').hasClass('validation')) {
-                $('<div class="dsn-form"><span class="validation">お支払方法の設定は必須です。</span></div>').insertBefore('div.dev-divider');
+            if( !$('div.dev-divider').prev('div').children('span').hasClass('billing-agreement')) {
+                $('<div class="dsn-form"><span class="validation billing-agreement">お支払方法の設定は必須です。</span></div>').insertBefore('div.dev-divider');
             }
 
             alert('Amazon Pay をお支払方法に設定する 同意は必須です。');
