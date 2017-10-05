7695c27a742514f35aef1d02e45c823d770f88fc
yoshida <yoshida.shota@terrada.co.jp>
2017-10-05 19:08:17 +0900

    refs #14469【AmazonPay初動】初回購入時の確認画面でamazonBillingAgreementIdが抜け落ちるエラーが発生している ・初回購入時のAmazonPayのModel変更の変更
    影響範囲：初回購入導線

---
+++ app/Model/Api/PaymentAmazonKitAmazonPayBillingAgreement.php
@@ -0,0 +1,186 @@
+<?php
+
+App::uses('ApiModel', 'Model');
+App::uses('Announcement', 'Model');
+App::uses('InfoBox', 'Model');
+
+class PaymentAmazonKitAmazonPayBillingAgreement extends ApiModel
+{
+    public function __construct()
+    {
+        parent::__construct('PaymentAmazonKitAmazonPayBillingAgreement', '/kit_amazon_pay_billing_agreement', 'amazon_pay_v4');
+    }
+
+    protected function triggerDataChanged()
+    {
+        parent::triggerDataChanged();
+        (new Announcement())->deleteCache();
+        (new InfoBox())->deleteCache();
+    }
+
+    public $validate = [
+        'mono_num' => [
+            'checkNotEmpty' => [
+                'rule' => 'checkNotEmpty',
+                'message' => ['checkNotEmpty', 'box'],
+            ],
+            'isStringInteger' => [
+                'rule' => 'isStringInteger',
+                'allowEmpty' => true,
+                'message' => ['format', 'kit_mono_num'],
+            ],
+        ],
+        'mono_appa_num' => [
+            'isStringInteger' => [
+                'rule' => 'isStringInteger',
+                'allowEmpty' => true,
+                'message' => ['format', 'kit_mono_num'],
+            ],
+        ],
+        'mono_book_num' => [
+            'isStringInteger' => [
+                'rule' => 'isStringInteger',
+                'allowEmpty' => true,
+                'message' => ['format', 'kit_mono_num'],
+            ],
+        ],
+        'hako_num' => [
+            'isStringInteger' => [
+                'rule' => 'isStringInteger',
+                'allowEmpty' => true,
+                'message' => ['format', 'kit_hako_num'],
+            ],
+        ],
+        'hako_appa_num' => [
+            'isStringInteger' => [
+                'rule' => 'isStringInteger',
+                'allowEmpty' => true,
+                'message' => ['format', 'kit_hako_num'],
+            ],
+        ],
+        'hako_book_num' => [
+            'isStringInteger' => [
+                'rule' => 'isStringInteger',
+                'allowEmpty' => true,
+                'message' => ['format', 'kit_hako_num'],
+            ],
+        ],
+        'cleaning_num' => [
+            'isStringInteger' => [
+                'rule' => 'isStringInteger',
+                'allowEmpty' => true,
+                'message' => ['format', 'kit_cleaning_num'],
+            ],
+        ],
+        'access_token' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'access_token'],
+            ],
+        ],
+        'amazon_user_id' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'amazon_user_id'],
+            ],
+        ],
+        'amazon_billing_agreement_id' => [
+            'notBlank' => [
+                'rule'     => 'notBlank',
+                'required' => true,
+                'message'  => ['notBlank', 'amazon_pay_id'],
+            ],
+            'isAmazonPayId' => [
+                'rule'     => 'isAmazonPayId',
+                'message'  => ['format', 'amazon_pay_id'],
+            ],
+        ],
+        'kit' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'kit'],
+            ],
+        ],
+        'name' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'kit_address_name']
+            ],
+            'maxLength' => [
+                'rule' => ['maxLength', 59],
+                'message' => ['maxLength', 'kit_address_name', 59]
+            ],
+            'isNotOnlySpace' => [
+                'rule' => 'isNotOnlySpace',
+                'message' => ['notBlank', 'staff_name']
+            ],
+        ],
+        'tel1' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'kit_tel1']
+            ],
+            'isPhoneNumberJp' => [
+                'rule' => 'isPhoneNumberJp',
+                'message' => ['format', 'kit_tel1']
+            ],
+        ],
+        'postal' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'kit_postal']
+            ],
+            'isPostalCodeJp' => [
+                'rule' => 'isPostalCodeJp',
+                'message' => ['format', 'kit_postal']
+            ],
+        ],
+        'address' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'kit_address']
+            ],
+            'maxLength' => [
+                'rule' => ['maxLength', 60],
+                'message' => ['maxLength', 'kit_address', 60]
+            ],
+            'isNotOnlySpace' => [
+                'rule' => 'isNotOnlySpace',
+                'message' => ['notBlank', 'staff_name']
+            ],
+        ],
+        'datetime_cd' => [
+            'notBlank' => [
+                'rule' => 'notBlank',
+                'required' => true,
+                'message' => ['notBlank', 'kit_datetime']
+            ],
+            'isDatetimeDelivery' => [
+                'rule' => 'isDatetimeDelivery',
+                'message' => ['format', 'kit_datetime']
+            ],
+        ],
+    ];
+
+    public function checkNotEmpty()
+    {
+        if (!empty($this->data[$this->model_name]['mono_num']) ||
+            !empty($this->data[$this->model_name]['mono_appa_num']) ||
+            !empty($this->data[$this->model_name]['mono_book_num']) ||
+            !empty($this->data[$this->model_name]['hako_num']) ||
+            !empty($this->data[$this->model_name]['hako_appa_num']) ||
+            !empty($this->data[$this->model_name]['hako_book_num']) ||
+            !empty($this->data[$this->model_name]['cleaning_num'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
