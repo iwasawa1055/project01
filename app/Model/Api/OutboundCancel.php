a42370d68493cc60a11de1e11ad3fc884fbce75b
sato <sato.takashi@terrada.co.jp>
2019-11-13 14:33:32 +0900

    refs #25427 入荷依頼履歴と出荷依頼履歴がほしい 　キャンセルAPIのエンドポイント変更

--- app/Model/Api/OutboundCancel.php
+++ app/Model/Api/OutboundCancel.php
@@ -6,8 +6,6 @@ class OutboundCancel extends ApiModel
 {
     public function __construct()
     {
-
-        // TODO 一旦paymentを使用する 後に変更する予定
-        parent::__construct('OutboundCancel', '/payment_outbound_cancel', 'minikura_v5');
+        parent::__construct('OutboundCancel', '/outbound_cancel', 'minikura_v5');
     }
 }
