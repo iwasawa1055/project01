48e0cbddf601951127178d0c02f9322dae7b40fd
sato <sato.takashi@terrada.co.jp>
2019-11-19 16:45:26 +0900

    refs #25427 入荷依頼履歴と出荷依頼履歴がほしい  キャンセルAPIのエラーメッセージを出力するように対応

--- app/Controller/OutboundHistoryController.php
+++ app/Controller/OutboundHistoryController.php
@@ -197,7 +197,7 @@ class OutboundHistoryController extends MinikuraController
         $this->OutboundCancel->set($this->request->data);
         $res = $this->OutboundCancel->apiPatch($this->OutboundCancel->toArray());
         if (!empty($res->error_message)) {
-            $this->Flash->set($res->error_message);
+            $this->Flash->validation($res->error_message, ['key' => 'data_error']);
             return $this->redirect(['controller' => 'outbound_history', 'action' => "detail?wl_id='{$work_linkage_id}'"]);
         }
 
